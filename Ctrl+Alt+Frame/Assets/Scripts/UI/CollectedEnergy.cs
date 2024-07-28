ea7d1d3316b3dd297b8575d8c4aa09c9587a9c17
TrackThTrail <henriquebarreira88@gmail.com>
2024-07-28 03:13:49 -0300

    ajuste energia canvas

--- Ctrl+Alt+Frame/Assets/Scripts/UI/CollectedEnergy.cs
+++ Ctrl+Alt+Frame/Assets/Scripts/UI/CollectedEnergy.cs
@@ -6,10 +6,16 @@ using TMPro;
 public class CollectedEnergy : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public GameManager gm;
 
     public void alterarEnergiaColetada(Component sender, object data)
     {
         if (data is int)
             text.text = data.ToString();
+    }
+
+    private void OnEnable()
+    {
+        text.text = gm.energyAmount.ToString();
     }
 }
